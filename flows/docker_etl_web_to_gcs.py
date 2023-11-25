84dd5f60e7f8123d84314c25f4e13891f3d57107
Le Duc Minh <minhmeo7a1@gmail.com>
2023-11-25 14:58:01 +0700

    Update docker flow code

--- flows/docker_etl_web_to_gcs.py
+++ flows/docker_etl_web_to_gcs.py
@@ -1,13 +1,13 @@
 from prefect.deployments import Deployment
 from prefect.infrastructure.docker import DockerContainer
-from parameterized_flow import etl_parent_flow
+from etl_web_to_gcs import etl_parent_web_to_gcs
 
 
-docker_block = DockerContainer.load("etl-docker")
+docker_block = DockerContainer.load("etl-web-to-gcs-docker")
 
 docker_dep = Deployment.build_from_flow(
-    flow=etl_parent_flow,
-    name="docker-flow",
+    flow=etl_parent_web_to_gcs,
+    name="docker-etl-web-to-gcs",
     infrastructure=docker_block
 )
 
