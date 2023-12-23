221aed1760c51c1d68802413a44ddc77ecba90ca
Le Duc Minh <minhmeo7a1@gmail.com>
2023-12-24 03:53:30 +0700

    Add option

--- kafka/streaming.py
+++ kafka/streaming.py
@@ -36,6 +36,7 @@ def read_from_kafka(consume_topic: str):
         .option("subscribe", consume_topic) \
         .option("startingOffsets", "earliest") \
         .option("checkpointLocation", "checkpoint") \
+        .option("failOnDataLoss", "false") \
         .load()
     return df_stream
 
