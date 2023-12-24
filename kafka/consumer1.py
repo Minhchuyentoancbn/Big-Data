ee226fa0bbc4d3b58a391ed29c983fc5ca882d3c
Le Duc Minh <minhmeo7a1@gmail.com>
2023-12-24 21:13:47 +0700

    Add consumer1

--- kafka/consumer1.py
+++ kafka/consumer1.py
@@ -1,12 +1,15 @@
 import argparse
+
+from json import loads
 from typing import Dict, List
 from kafka import KafkaConsumer
+from .ride import Ride
 
-from settings import CONSUME_TOPIC_RIDES_CSV
+CONSUME_TOPIC = 'rides_json'
 
 BOOTSTRAP_SERVERS = ['35.220.200.137:9092', ]
 
-class RideCSVConsumer:
+class RideJSONConsumer:
     def __init__(self, props: Dict):
         self.consumer = KafkaConsumer(**props)
 
@@ -32,7 +35,7 @@ class RideCSVConsumer:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Kafka Consumer')
-    parser.add_argument('--topic', type=str, default=CONSUME_TOPIC_RIDES_CSV)
+    parser.add_argument('--topic', type=str, default=CONSUME_TOPIC)
     args = parser.parse_args()
 
     topic = args.topic
@@ -41,8 +44,32 @@ if __name__ == '__main__':
         'auto_offset_reset': 'earliest',
         'enable_auto_commit': True,
         'key_deserializer': lambda key: key.decode('utf-8'),
-        'value_deserializer': lambda value: value.decode('utf-8'),
+        'value_deserializer': lambda x: loads(x.decode('utf-8'), object_hook=lambda d: Ride.from_dict(d)),
         'group_id': 'consumer.group.id.csv-example.1',
     }
-    csv_consumer = RideCSVConsumer(props=config)
+    csv_consumer = RideJSONConsumer(props=config)
     csv_consumer.consume_from_kafka(topics=[topic])
+
+
+# class RideCSVConsumer:
+#     def __init__(self, props: Dict):
+#         self.consumer = KafkaConsumer(**props)
+
+#     def consume_from_kafka(self, topics: List[str]):
+#         self.consumer.subscribe(topics=topics)
+#         print('Consuming from Kafka started')
+#         print('Available topics to consume: ', self.consumer.subscription())
+#         while True:
+#             try:
+#                 # SIGINT can't be handled when polling, limit timeout to 1 second.
+#                 msg = self.consumer.poll(1.0)
+#                 if msg is None or msg == {}:
+#                     continue
+#                 for msg_key, msg_values in msg.items():
+#                     for msg_val in msg_values:
+#                         print(f'Key:{msg_val.key}-type({type(msg_val.key)}), '
+#                               f'Value:{msg_val.value}-type({type(msg_val.value)})')
+#             except KeyboardInterrupt:
+#                 break
+
+#         self.consumer.close()
