d547b2a9024d6f259c8ff34ddc6b23156fb47552
Le Duc Minh <minhmeo7a1@gmail.com>
2023-12-24 19:40:17 +0700

    Add 2 producers

---
+++ kafka/producer2.py
@@ -0,0 +1,72 @@
+import csv
+import sys
+import argparse
+from time import sleep
+from typing import Dict
+from kafka import KafkaProducer
+
+from settings import PRODUCE_TOPIC_RIDES_CSV
+
+INPUT_DATA_PATH = './resources/rides2.csv'
+BOOTSTRAP_SERVERS = ['35.220.200.137:9093', ]
+
+def delivery_report(err, msg):
+    if err is not None:
+        print("Delivery failed for record {}: {}".format(msg.key(), err))
+        return
+    print('Record {} successfully produced to {} [{}] at offset {}'.format(
+        msg.key(), msg.topic(), msg.partition(), msg.offset()))
+
+
+class RideCSVProducer:
+    def __init__(self, props: Dict):
+        self.producer = KafkaProducer(**props)
+        # self.producer = Producer(producer_props)
+
+    @staticmethod
+    def read_records(resource_path: str):
+        records, ride_keys = [], []
+        with open(resource_path, 'r') as f:
+            reader = csv.reader(f)
+            header = next(reader)  # skip the header
+            i = 0
+            for row in reader:
+                # vendor_id, passenger_count, trip_distance, payment_type, total_amount
+                records.append(f'{row[0]}, {row[1]}, {row[2]}, {row[3]}, {row[4]}, {row[5]}, {row[6]}, {row[7]}, {row[8]}, {row[9]}, {row[10]}, {row[11]}, {row[12]}, {row[13]}, {row[14]}, {row[15]}, {row[16]}, {row[17]}')
+                ride_keys.append(str(row[1]) + '-' + str(row[7]))
+                i += 1
+                if i == 1000:
+                    break
+        return zip(ride_keys, records)
+
+    def publish(self, topic: str, records: [str, str], sleep_time: float = 0.5):
+        for key_value in records:
+            key, value = key_value
+            try:
+                self.producer.send(topic=topic, key=key, value=value)
+                print(f"Producing record for <key: {key}, value:{value}>")
+            except KeyboardInterrupt:
+                break
+            except Exception as e:
+                print(f"Exception while producing record - {value}: {e}") 
+
+            sleep(sleep_time)
+
+        self.producer.flush()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--time', type=float, default=0.5, help='time interval between each message')
+    args = parser.parse_args(sys.argv[1:])
+
+    config = {
+        'bootstrap_servers': BOOTSTRAP_SERVERS,
+        'key_serializer': lambda x: x.encode('utf-8'),
+        'value_serializer': lambda x: x.encode('utf-8'),
+        # 'acks': 'all',
+    }
+    producer = RideCSVProducer(props=config)
+    ride_records = producer.read_records(resource_path=INPUT_DATA_PATH)
+    print(ride_records)
+    producer.publish(topic=PRODUCE_TOPIC_RIDES_CSV, records=ride_records, sleep_time=args.time)
--- kafka/streaming.py
+++ kafka/streaming.py
@@ -6,7 +6,7 @@ import os
 
 KAFKA_ADDRESS= "35.220.200.137"
 TOPIC_WINDOWED_VENDOR_ID_COUNT = 'vendor_counts_windowed_2'
-PRODUCE_TOPIC_RIDES_CSV = CONSUME_TOPIC_RIDES_CSV = 'rides_csv_3'
+PRODUCE_TOPIC_RIDES_CSV = CONSUME_TOPIC_RIDES_CSV = 'rides_csv_4'
 
 GCP_GCS_BUCKET = "dtc_data_lake_bigdata-405714"
 GCS_STORAGE_PATH = 'gs://' + GCP_GCS_BUCKET + '/realtime'
